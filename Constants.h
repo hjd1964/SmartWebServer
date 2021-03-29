f4de5d093f6f32b4ee65dd7ec0c2ec7fa1adcdb5
Howard Dutton <hjd1964@gmail.com>
2021-03-29 13:16:26 -0400

    Upped patch level

--- Constants.h
+++ Constants.h
@@ -5,7 +5,7 @@
 #define Product "Smart Web Server"
 #define FirmwareVersionMajor  "0"
 #define FirmwareVersionMinor  "5"
-#define FirmwareVersionPatch  "b"
+#define FirmwareVersionPatch  "c"
 #define FirmwareDate     __DATE__
 #define FirmwareTime     __TIME__
 
