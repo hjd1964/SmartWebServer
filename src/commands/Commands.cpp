2cf375a5a60c6ce18789cd4139c4621cdb06a04b
Howard Dutton <hjd1964@gmail.com>
2021-04-13 08:28:39 -0400

    Refactoring

--- src/commands/Commands.cpp
+++ src/commands/Commands.cpp
@@ -93,90 +93,90 @@ bool processCommand(const char* cmd, char* response, long timeOutMs) {
   // send the command
   Ser.print(cmd);
 
-  bool noResponse=false;
-  bool shortResponse=false;
-  if ((cmd[0]==(char)6) && (cmd[1]==0)) shortResponse=true;
-  if ((cmd[0]==':') || (cmd[0]==';')) {
-    if (cmd[1]=='G') {
-      if (strchr("RDE",cmd[2])) { if (timeOutMs < 300) timeOutMs = 300; }
+  bool noResponse = false;
+  bool shortResponse = false;
+  if (cmd[0] == (char)6 && cmd[1] == 0) shortResponse = true;
+  if (cmd[0] == ':' || cmd[0] == ';') {
+    if (cmd[1] == 'G') {
+      if (strchr("RDE", cmd[2])) { if (timeOutMs < 300) timeOutMs = 300; }
     } else
-    if (cmd[1]=='M') {
-      if (strchr("ewnsg",cmd[2])) noResponse=true;
-      if (strchr("ADNPS",cmd[2])) shortResponse=true;
+    if (cmd[1] == 'M') {
+      if (strchr("ewnsg", cmd[2])) noResponse = true;
+      if (strchr("ADNPS", cmd[2])) shortResponse = true;
     } else
-    if (cmd[1]=='Q') {
-      if (strchr("#ewns",cmd[2])) noResponse=true;
+    if (cmd[1] == 'Q') {
+      if (strchr("#ewns", cmd[2])) noResponse = true;
     } else
-    if (cmd[1]=='A') {
-      if (strchr("W123456789+",cmd[2])) { shortResponse=true; if (timeOutMs < 1000) timeOutMs = 1000; }
+    if (cmd[1] == 'A') {
+      if (strchr("W123456789+",cmd[2])) { shortResponse = true; if (timeOutMs < 1000) timeOutMs = 1000; }
     } else
-    if ((cmd[1]=='F') || (cmd[1]=='f')) {
-      if (strchr("+-QZHhF1234",cmd[2])) noResponse=true;
-      if (strchr("Ap",cmd[2])) shortResponse=true;
+    if ((cmd[1] == 'F') || (cmd[1] == 'f')) {
+      if (strchr("+-QZHhF1234", cmd[2])) noResponse = true;
+      if (strchr("Ap",cmd[2])) shortResponse = true;
     } else
-    if (cmd[1]=='r') {
-      if (strchr("+-PRFC<>Q1234",cmd[2])) noResponse=true;
-      if (strchr("~S",cmd[2])) shortResponse=true;
+    if (cmd[1] == 'r') {
+      if (strchr("+-PRFC<>Q1234", cmd[2])) noResponse = true;
+      if (strchr("~S",cmd[2])) shortResponse = true;
     } else
     if (cmd[1] == 'R') {
-      if (strchr("AEGCMS0123456789", cmd[2])) noResponse=true;
+      if (strchr("AEGCMS0123456789", cmd[2])) noResponse = true;
     } else
     if (cmd[1]=='S') {
-      if (strchr("CLSGtgMNOPrdhoTBX",cmd[2])) shortResponse=true;
+      if (strchr("CLSGtgMNOPrdhoTBX", cmd[2])) shortResponse = true;
     } else
     if (cmd[1]=='L') {
-      if (strchr("BNCDL!",cmd[2])) noResponse=true;
-      if (strchr("o$W",cmd[2])) { shortResponse=true; if (timeOutMs < 1000) timeOutMs = 1000; }
+      if (strchr("BNCDL!",cmd[2])) noResponse = true;
+      if (strchr("o$W", cmd[2])) { shortResponse = true; if (timeOutMs < 1000) timeOutMs = 1000; }
     } else
     if (cmd[1]=='B') {
-      if (strchr("+-",cmd[2])) noResponse=true;
+      if (strchr("+-", cmd[2])) noResponse = true;
     } else
     if (cmd[1]=='C') {
-      if (strchr("S",cmd[2])) noResponse=true;
+      if (strchr("S", cmd[2])) noResponse = true;
     } else
     if (cmd[1]=='h') {
-      if (strchr("FC",cmd[2])) { noResponse=true; if (timeOutMs < 1000) timeOutMs = 1000; }
-      if (strchr("QPR",cmd[2])) { shortResponse=true; if (timeOutMs < 300) timeOutMs = 300; }
+      if (strchr("FC", cmd[2])) { noResponse = true; if (timeOutMs < 1000) timeOutMs = 1000; }
+      if (strchr("QPR", cmd[2])) { shortResponse = true; if (timeOutMs < 300) timeOutMs = 300; }
     } else
     if (cmd[1]=='T') {
-      if (strchr("QR+-SLK",cmd[2])) noResponse=true;
-      if (strchr("edrn",cmd[2])) shortResponse=true;
+      if (strchr("QR+-SLK", cmd[2])) noResponse = true;
+      if (strchr("edrn", cmd[2])) shortResponse = true;
     } else
     if (cmd[1]=='U') {
-      noResponse=true; 
+      noResponse = true; 
     } else
     if (cmd[1]=='W') {
-      if (strchr("R",cmd[2])) {
-        if (strchr("+-",cmd[3])) shortResponse=true; else noResponse=true; // WR+ WR- else WR
+      if (strchr("R", cmd[2])) {
+        if (strchr("+-", cmd[3])) shortResponse = true; else noResponse = true; // WR+ WR- else WR
       }
-      if (strchr("S",cmd[2])) shortResponse=true;  // WS
-      if (strchr("0123",cmd[2])) noResponse=true;  // W0 W1 W2 W3
+      if (strchr("S", cmd[2])) shortResponse = true;  // WS
+      if (strchr("0123", cmd[2])) noResponse = true;  // W0 W1 W2 W3
     } else
-    if ((cmd[1]=='$') && (cmd[2]=='Q') && (cmd[3]=='Z')) {
-      if (strchr("+-Z/!",cmd[4])) noResponse=true;
+    if (cmd[1] == '$' && cmd[2] == 'Q' && cmd[3] == 'Z') {
+      if (strchr("+-Z/!", cmd[4])) noResponse = true;
     }
 
     // override for checksum protocol
-    if (cmd[0]==';') { noResponse=false; shortResponse=false; }
+    if (cmd[0] == ';') { noResponse = false; shortResponse = false; }
   }
 
   unsigned long timeout = millis()+(unsigned long)timeOutMs;
   if (noResponse) {
-    response[0]=0;
+    response[0] = 0;
     return true;
   } else
   if (shortResponse) {
     while ((long)(timeout - millis()) > 0) {
-      if (Ser.available()) { response[Ser.readBytes(response,1)]=0; break; }
+      if (Ser.available()) { response[Ser.readBytes(response, 1)] = 0; break; }
     }
-    return (response[0]!=0);
+    return (response[0] != 0);
   } else {
     // get full response, '#' terminated
-    int responsePos=0;
-    char b=0;
+    int responsePos = 0;
+    char b = 0;
     while ((long)(timeout - millis()) > 0 && b != '#') {
       if (Ser.available()) {
-        b=Ser.read();
+        b = Ser.read();
         response[responsePos] = b; responsePos++; if (responsePos > 39) responsePos = 39; response[responsePos] = 0;
       }
     }
@@ -186,42 +186,43 @@ bool processCommand(const char* cmd, char* response, long timeOutMs) {
 
 bool command(const char* command, char* response) {
   bool success = processCommand(command, response, webTimeout);
-  int l = strlen(response)-1; if (l >= 0 && response[l] == '#') response[l] = 0;
+  int l = strlen(response) - 1;
+  if (l >= 0 && response[l] == '#') response[l] = 0;
   return success;
 }
 
 bool commandBlind(const char* command) {
-  char response[40]="";
+  char response[40] = "";
   return processCommand(command, response, webTimeout);
 }
 
 bool commandEcho(const char* command) {
-  char response[40]="";
-  char c[40]="";
-  sprintf(c,":EC%s#",command);
+  char response[40] = "";
+  char c[40] = "";
+  sprintf(c, ":EC%s#", command);
   return processCommand(c, response, webTimeout);
 }
 
 bool commandBool(const char* command) {
-  char response[40]="";
+  char response[40] = "";
   bool success = processCommand(command,response,webTimeout);
-  int l=strlen(response)-1; if (l >= 0 && response[l] == '#') response[l]=0;
+  int l = strlen(response) - 1; if (l >= 0 && response[l] == '#') response[l] = 0;
   if (!success) return false;
   if (response[1] != 0) return false;
   if (response[0] == '0') return false; else return true;
 }
 
 char *commandString(const char* command) {
-  static char response[40]="";
+  static char response[40] = "";
   bool success = processCommand(command,response,webTimeout);
-  int l=strlen(response)-1; if (l >= 0 && response[l] == '#') response[l]=0;
+  int l = strlen(response) - 1; if (l >= 0 && response[l] == '#') response[l] = 0;
   if (!success) strcpy(response,"?");
   return response;
 }
 
 char* commandErrorToStr(int e) {
   static char reply[40];
-  strcpy(reply,"Error, ");
+  strcpy(reply, "Error, ");
   
   switch (e) {
     case CE_NONE: strcpy(reply, L_CE_NONE); break;
