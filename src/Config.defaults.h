5f1a3d0bbedb52caf96a5282ab3ec263277934f5
Howard Dutton <hjd1964@gmail.com>
2021-11-11 19:04:31 -0500

    Formatting

--- src/Config.defaults.h
+++ src/Config.defaults.h
@@ -3,4 +3,4 @@
 #pragma once
 
 // Wifi related
-#define STA_AP_FALLBACK              true // activate SoftAP if station fails to connect
+#define STA_AP_FALLBACK                true // activate SoftAP if station fails to connect
