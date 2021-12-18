a4ae8dc13186ef90f58ae93e193f17b0d57149d2
Howard Dutton <hjd1964@gmail.com>
2021-12-17 19:07:50 -0500

    Upped minor revision

--- SmartWebServer.ino
+++ SmartWebServer.ino
@@ -31,8 +31,8 @@
 
 #define Product "Smart Web Server"
 #define FirmwareVersionMajor  "2"
-#define FirmwareVersionMinor  "01"
-#define FirmwareVersionPatch  "e"
+#define FirmwareVersionMinor  "02"
+#define FirmwareVersionPatch  "a"
 
 // Use Config.h to configure the SWS to your requirements
 
