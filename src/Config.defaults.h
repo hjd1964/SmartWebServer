53644a4d93a7482f197ab26d9b8b1a0ed82d5fc4
Howard Dutton <hjd1964@gmail.com>
2021-12-29 20:30:31 -0500

    Added defaults for RESET_PIN

--- src/Config.defaults.h
+++ src/Config.defaults.h
@@ -4,3 +4,12 @@
 
 // Wifi related
 #define STA_AP_FALLBACK                true // activate SoftAP if station fails to connect
+
+// onstep reset control related
+#ifndef RESET_PIN
+  #define RESET_PIN                    OFF
+#endif
+
+#ifndef RESET_PIN_STATE
+  #define RESET_PIN_STATE              LOW
+#endif
