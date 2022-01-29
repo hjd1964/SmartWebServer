e12e7da47ef2d18b7f09b012d5a76266005c8b5d
Howard Dutton <hjd1964@gmail.com>
2022-01-28 20:08:31 -0500

    Allow NV endurance from config file

--- src/HAL/HAL_ESP32.h
+++ src/HAL/HAL_ESP32.h
@@ -15,7 +15,6 @@
 // Non-volatile storage ------------------------------------------------------------------------------
 #if NV_DRIVER == NV_DEFAULT
   #define E2END 1023
-  #undef  NV_ENDURANCE
   #define NV_ENDURANCE NVE_LOW
   #include "../lib/nv/NV_ESP.h"
   #define HAL_NV_INIT { nv.init(E2END + 1, false, 1000, false); }
