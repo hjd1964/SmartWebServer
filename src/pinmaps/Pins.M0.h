34c5cce1038858cb404f09fdf25d222d29549505
Howard Dutton <hjd1964@gmail.com>
2022-05-12 18:15:26 -0400

    Use pinmaps to auto exclude encoder pins

--- src/pinmaps/Pins.M0.h
+++ src/pinmaps/Pins.M0.h
@@ -12,10 +12,17 @@
   #define AXIS2_ENC_A_PIN    7   // pin# for Axis2 encoder, for A or CW  or MA/CLK
   #define AXIS2_ENC_B_PIN    8   // pin# for Axis2 encoder, for B or CCW or SLO
 
-  #define VGPIO_PIN_0        AXIS1_ENC_A_PIN
-  #define VGPIO_PIN_1        AXIS1_ENC_B_PIN
-  #define VGPIO_PIN_2        AXIS2_ENC_A_PIN
-  #define VGPIO_PIN_3        AXIS2_ENC_B_PIN
+  #if AXIS1_ENC > 0 && AXIS2_ENC > 0
+    #define VGPIO_PIN_0      OFF
+    #define VGPIO_PIN_1      OFF
+    #define VGPIO_PIN_2      OFF
+    #define VGPIO_PIN_3      OFF
+  #else
+    #define VGPIO_PIN_0      AXIS1_ENC_A_PIN
+    #define VGPIO_PIN_1      AXIS1_ENC_B_PIN
+    #define VGPIO_PIN_2      AXIS2_ENC_A_PIN
+    #define VGPIO_PIN_3      AXIS2_ENC_B_PIN
+  #endif
   #define VGPIO_PIN_4        OFF
   #define VGPIO_PIN_5        OFF
   #define VGPIO_PIN_6        OFF
