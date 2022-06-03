5295a708616b8f90f307bfab330f1cf1905546da
Howard Dutton <hjd1964@gmail.com>
2022-06-03 14:21:14 -0400

    Added ability to change HAL_WIRE_CLOCK from Config.h

--- src/HAL/HAL_MISC.h
+++ src/HAL/HAL_MISC.h
@@ -6,7 +6,9 @@
 // New symbol for the default I2C port ---------------------------------------------------------------
 #include <Wire.h>
 #define HAL_Wire Wire
-#define HAL_WIRE_CLOCK 100000
+#ifndef HAL_WIRE_CLOCK
+  #define HAL_WIRE_CLOCK 100000
+#endif
 
 // Default serial port where OnStep is attached ------------------------------------------------------
 #define SERIAL_ONSTEP Serial
