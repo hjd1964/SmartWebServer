5b28884cb10b6ba5a2218d8e3d2ae138d2853afc
Howard Dutton <hjd1964@gmail.com>
2022-07-05 08:13:49 -0400

    Fixed home/park state issues

--- src/pages/mount/HomeParkTile.cpp
+++ src/pages/mount/HomeParkTile.cpp
@@ -42,6 +42,15 @@ void homeParkTile(String &data)
 void homeParkTileAjax(String &data)
 {
   data.concat(keyValueString("hp_sta", getHomeParkStateStr()));
+
+  if (status.atHome || status.parked) {
+    data.concat(keyValueBoolEnabled("park", false));
+    data.concat(keyValueBoolEnabled("unpark", true));
+  } else {
+    data.concat(keyValueBoolEnabled("park", !(status.parkFail || status.parking)));
+    data.concat(keyValueBoolEnabled("unpark", false));
+  }
+    
   www.sendContentAndClear(data);
 }
 
@@ -53,44 +62,35 @@ void homeParkTileGet()
   v = www.arg("pk");
   if (!v.equals(EmptyStr))
   {
-    if (v.equals("p"))
-      onStep.commandBool(":hP#"); // park
-    if (v.equals("u"))
-      onStep.commandBool(":hR#"); // un-park
-    if (v.equals("s"))
-      onStep.commandBool(":hQ#"); // set-park
+    if (v.equals("p")) onStep.commandBool(":hP#"); // park
+    if (v.equals("u")) onStep.commandBool(":hR#"); // un-park
+    if (v.equals("s")) onStep.commandBool(":hQ#"); // set-park
   }
 
   v = www.arg("qb");
   if (!v.equals(EmptyStr))
   {
-    if (v.equals("hf"))
-      onStep.commandBlind(":hF#"); // home, reset
-    if (v.equals("hc"))
-      onStep.commandBlind(":hC#"); // home, find
+    if (v.equals("hf")) onStep.commandBlind(":hF#"); // home, reset
+    if (v.equals("hc")) onStep.commandBlind(":hC#"); // home, find
   }
 
   v = www.arg("dr");
   if (!v.equals(EmptyStr))
   {
-    if (v.equals("qr"))
-      onStep.commandBlind(":hF#"); // home, reset
-    if (v.equals("qh"))
-      onStep.commandBlind(":hC#"); // home, find
-    if (v.equals("pk"))
-      onStep.commandBool(":hP#"); // park
-    if (v.equals("pu"))
-      onStep.commandBool(":hR#"); // un-park
+    if (v.equals("qr")) onStep.commandBlind(":hF#"); // home, reset
+    if (v.equals("qh")) onStep.commandBlind(":hC#"); // home, find
+    if (v.equals("pk")) onStep.commandBool(":hP#"); // park
+    if (v.equals("pu")) onStep.commandBool(":hR#"); // un-park
   }
 }
 
 const char *getHomeParkStateStr()
 {
-  static char hsta[32] = L_INACTIVE;
-  if (status.parking) strcpy(hsta, L_PARKING); else
-  if (status.homing) strcpy(hsta, L_HOMING); else
-  if (status.parkFail) strcpy(hsta, L_PARK_FAILED); else
-  if (status.parked) strcpy(hsta, L_PARKED); else
-  if (status.atHome) strcpy(hsta, L_AT_HOME);
+  static char hsta[32];
+  if (status.parking) strncpyex(hsta, L_PARKING, 32); else
+  if (status.homing) strncpyex(hsta, L_HOMING, 32); else
+  if (status.parkFail) strncpyex(hsta, L_PARK_FAILED, 32); else
+  if (status.parked) strncpyex(hsta, L_PARKED, 32); else
+  if (status.atHome) strncpyex(hsta, L_AT_HOME, 32); else strncpyex(hsta, L_INACTIVE, 32);
   return hsta;
 }
