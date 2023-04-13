ce6011b797ed26c40677d36b336138b59908ade3
Howard Dutton <hjd1964@gmail.com>
2023-04-13 15:25:06 -0400

    Added encoder configuration defaults

--- src/Config.defaults.h
+++ src/Config.defaults.h
@@ -46,3 +46,52 @@
 #ifndef STATE_GPIO_POLLING_RATE_MS
 #define STATE_GPIO_POLLING_RATE_MS    100     // time between updates of OnStep GPIO pin state
 #endif
+
+// encoders
+#ifndef ENC_AUTO_SYNC_DEFAULT
+#define ENC_AUTO_SYNC_DEFAULT         ON      // automatically sync Encoders to OnStep
+#endif
+#ifndef ENC_AUTO_SYNC_MEMORY
+#define ENC_AUTO_SYNC_MEMORY          OFF     // remember automatic sync setting across power cycles
+#endif
+#ifndef ENC_SYNC_DURING_ALIGN
+#define ENC_SYNC_DURING_ALIGN         OFF     // high resolution encoders correct pointing even during an align
+#endif
+
+#ifndef AXIS1_ENCODER
+#define AXIS1_ENCODER                 OFF     // AB, AB_ESP32, CW_CCW, PULSE_DIR, AS37_H39B_B. RA/Azm (A/MA) & (B/SLO)
+#endif
+#ifndef AXIS1_ENCODER_TICKS_DEG
+#define AXIS1_ENCODER_TICKS_DEG       22.222  // encoder ticks per degree
+#endif
+#ifndef AXIS1_ENCODER_REVERSE
+#define AXIS1_ENCODER_REVERSE         OFF     // reverse the count direction
+#endif
+#ifndef AXIS1_ENCODER_DIFF_LIMIT_TO
+#define AXIS1_ENCODER_DIFF_LIMIT_TO   0       // minimum diff. between encoder/OnStep for sync. to OnStep
+#endif
+#ifndef AXIS1_ENCODER_DIFF_LIMIT_FROM
+#define AXIS1_ENCODER_DIFF_LIMIT_FROM 0       // maximum diff. between encoder/OnStep for sync. from OnStep
+#endif
+
+#ifndef AXIS2_ENCODER
+#define AXIS2_ENCODER                 OFF     // AB, AB_ESP32, CW_CCW, PULSE_DIR, AS37_H39B_B. RA/Azm (A/MA) & (B/SLO)
+#endif
+#ifndef AXIS2_ENCODER_TICKS_DEG
+#define AXIS2_ENCODER_TICKS_DEG       22.222  // encoder ticks per degree
+#endif
+#ifndef AXIS2_ENCODER_REVERSE
+#define AXIS2_ENCODER_REVERSE         OFF     // reverse the count direction
+#endif
+#ifndef AXIS2_ENCODER_DIFF_LIMIT_TO
+#define AXIS2_ENCODER_DIFF_LIMIT_TO   0       // minimum diff. between encoder/OnStep for sync. to OnStep
+#endif
+#ifndef AXIS2_ENCODER_DIFF_LIMIT_FROM
+#define AXIS2_ENCODER_DIFF_LIMIT_FROM 0       // maximum diff. between encoder/OnStep for sync. from OnStep
+#endif
+
+// AS37 absolute encoder
+//#define AS37_SINGLE_TURN                    // uncomment to enable AS37 single turn mode (no multi-turn count)
+#ifndef AS37_CLOCK_RATE_KHZ
+#define AS37_CLOCK_RATE_KHZ           4000    // error rate is: ~2.0% at 500 KHz, ~0.5% @ 2000 KHz, ~0.01% at 4000KHz
+#endif
