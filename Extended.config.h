a3568f6273ec188fcc889e0de8603bac13a2e6cb
drewbolce <drewbolce@users.noreply.github.com>
2023-07-04 18:45:51 -0400

    Update Extended.config.h
    Refactoring

--- Extended.config.h
+++ Extended.config.h
@@ -32,12 +32,12 @@
 #define PASSWORD_DEFAULT       "password" //  "password", Adjust as required, this can be changed at runtime also.            Adjust
                                           //              password for runtime access to network settings.
 
-// BLE GAMEPAD SETTINGS (ESP32 ONLY) ------------------------------------------- see https://onstep.groups.io/g/main/wiki/26762
-#define BLE_GAMEPAD                   OFF //         OFF, ON to allow BLE gamepad connection for ESP32 only.                   Option
-#define BLE_GP_ADDR   "ff:ff:de:09:f5:cf" // ...f:ff:ff", GamePad MAC address #1                                               Adjust
+// BLE GAMEPAD SETTINGS (ESP32 ONLY) ------------------------------------------------ see https://onstep.groups.io/g/main/wiki/26762
+#define BLE_GAMEPAD                   OFF //         OFF, ON to allow BLE gamepad connection for ESP32 only.                  Option
+#define BLE_GP_ADDR   "ff:ff:de:09:f5:cf" // ...f:ff:ff", GamePad MAC address #1                                              Adjust
                                           //              GamePad device address must match exactly else it will not connect!
                                           //              Replace address with yours, in lower case, with colons as shown.
-#define BLE_GP_ADDR1  "ff:ff:ff:ff:ff:ff" // ...f:ff:ff", GamePad MAC address #2                                               Adjust
+#define BLE_GP_ADDR1  "ff:ff:ff:ff:ff:ff" // ...f:ff:ff", GamePad MAC address #2                                              Adjust
                                           //              Two GamePads are allowed, operating in a "handoff" mode, one at time.
 
 // IP ADDRESS SETTINGS -------------------------------------------------------------------------------------------------------------
