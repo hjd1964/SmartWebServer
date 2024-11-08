5c8baefab547eabfd8fd05ca38d96653c7fb0646
Howard Dutton <hjd1964@gmail.com>
2024-11-08 07:29:29 -0500

    Changed ESP32-C3 pinmap

--- src/pinmaps/Pins.Esp32c3.h
+++ src/pinmaps/Pins.Esp32c3.h
@@ -1,34 +1,34 @@
 // -------------------------------------------------------------------------------------------------
-// Pin map for ESP32C3
+// Pin map for ESP32C3 LilyGO T-01C3 (experimental)
 #pragma once
 
-// locate WeMos D1 R32 serial port pins to match WeMos D1 Mini main and swapped port pins
+// default serial port pins and no swapped port pins
 #ifndef SERIAL_RX
-  #define SERIAL_RX          1
+  #define SERIAL_RX          20
 #endif
 #ifndef SERIAL_TX
-  #define SERIAL_TX          3
+  #define SERIAL_TX          21
 #endif
 #ifndef SERIAL_SWAPPED_RX
-  #define SERIAL_SWAPPED_RX  23
+  #define SERIAL_SWAPPED_RX  OFF
 #endif
 #ifndef SERIAL_SWAPPED_TX
-  #define SERIAL_SWAPPED_TX  5
+  #define SERIAL_SWAPPED_TX  OFF
 #endif
 
-#ifndef LED_STATUS_PIN      
-  #define LED_STATUS_PIN     2   // pin 2 is a guess and is probably wrong
+#ifndef LED_STATUS_PIN
+  #define LED_STATUS_PIN     3
 #endif
 
-#define BOOT0_PIN            5   // GPIO5 to Boot0 of STM32 etc. (no swapped serial if active)
+#define BOOT0_PIN            OFF // to Boot0 of STM32 etc. (no swapped serial if active)
 
-#define ETHERNET_RESET_PIN   9   // pin# for controlling the reset of W5500 so it comes up properly
+#define ETHERNET_RESET_PIN   OFF // pin# for controlling the reset of W5500 so it comes up properly
 #define SD_CARD_CS_PIN       OFF // pin 4 to CS for SD adapter on Arduino Ethernet Shield
 
-#define AXIS1_ENCODER_A_PIN  18  // pin# for Axis1 encoder, for A or CW  or MA/CLK
-#define AXIS1_ENCODER_B_PIN  19  // pin# for Axis1 encoder, for B or CCW or SLO
-#define AXIS2_ENCODER_A_PIN  22  // pin# for Axis1 encoder, for A or CW  or MA/CLK
-#define AXIS2_ENCODER_B_PIN  21  // pin# for Axis1 encoder, for B or CCW or SLO
+#define AXIS1_ENCODER_A_PIN  OFF // pin# for Axis1 encoder, for A or CW  or MA/CLK
+#define AXIS1_ENCODER_B_PIN  OFF // pin# for Axis1 encoder, for B or CCW or SLO
+#define AXIS2_ENCODER_A_PIN  OFF // pin# for Axis1 encoder, for A or CW  or MA/CLK
+#define AXIS2_ENCODER_B_PIN  OFF // pin# for Axis1 encoder, for B or CCW or SLO
 
 #if AXIS1_ENCODER == OFF || AXIS2_ENCODER == OFF
   #define VGPIO_PIN_0        AXIS1_ENCODER_A_PIN
