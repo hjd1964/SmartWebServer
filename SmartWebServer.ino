f2659829066f60f04e9153b77ad0f0282a7cbda2
Howard Dutton <hjd1964@gmail.com>
2026-01-14 14:40:58 -0500

    Upped patch level

--- SmartWebServer.ino
+++ SmartWebServer.ino
@@ -32,7 +32,7 @@
 #define Product "Smart Web Server"
 #define FirmwareVersionMajor  "2"
 #define FirmwareVersionMinor  "09"
-#define FirmwareVersionPatch  "d"
+#define FirmwareVersionPatch  "e"
 
 // Use Config.h to configure the SWS to your requirements
 
